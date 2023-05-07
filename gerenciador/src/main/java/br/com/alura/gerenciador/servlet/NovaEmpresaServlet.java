69f48f3150b8734f7d59f81c706e1197b0d7d143
Hebert <hebertLandim007@outlook.com>
2023-05-07 11:46:04 -0300

    Redirecting the flow

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -5,7 +5,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,28 +23,33 @@ public class NovaEmpresaServlet extends HttpServlet {
 			throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa");
 		
-        String nomeEmpresa = request.getParameter("nome");
-        String paramDataEmpresa = request.getParameter("data");
-        
-        Date dataAbertura = null;
-        try {
-        	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		String nomeEmpresa = request.getParameter("nome");
+		String paramDataEmpresa = request.getParameter("data");
+		
+		Date dataAbertura = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 			dataAbertura = sdf.parse(paramDataEmpresa);
 		} catch (ParseException e) {
 			throw new ServletException(e);
 		}
-        
-        Empresa empresa = new Empresa();
-        empresa.setNome(nomeEmpresa);
-        empresa.setDataAbertura(dataAbertura);
-
-        Banco banco = new Banco();
-        banco.adiciona(empresa);
-
-        //chamar o JSP
-        RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
-        request.setAttribute("empresa", empresa.getNome());
-        rd.forward(request, response);
+		
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
+		empresa.setDataAbertura(dataAbertura);
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
+		
+		request.setAttribute("empresa", empresa.getNome());
+		
+		response.sendRedirect("listaEmpresas");
+		
+//		//chamar o JSP ou Servlet
+//		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas");
+//		request.setAttribute("empresa", empresa.getNome());
+//		rd.forward(request, response);
 	}
 
 }
+
--- gerenciador/src/main/webapp/listaEmpresas.jsp
+++ gerenciador/src/main/webapp/listaEmpresas.jsp
@@ -1,7 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-	pageEncoding="ISO-8859-1"%>
-<%@ page
-	import="java.util.List, br.com.alura.gerenciador.servlet.Empresa"%>
+    pageEncoding="ISO-8859-1"%>
+<%@ page import="java.util.List, br.com.alura.gerenciador.servlet.Empresa"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
 
@@ -12,14 +11,19 @@
 <title>Java Standard Taglib</title>
 </head>
 <body>
-	Lista de empresas:
-	<br />
 
+	<c:if test="${not empty empresa}">
+		Empresa ${ empresa } cadastrada com sucesso!
+	</c:if>
+	
+	Lista de empresas: <br />
+	
 	<ul>
 		<c:forEach items="${empresas}" var="empresa">
-			 <li>${empresa.nome} - <fmt:formatDate value="${empresa.dataAbertura}" pattern="dd/MM/yyyy"/> </li>
+			
+			<li>${empresa.nome } - <fmt:formatDate value="${empresa.dataAbertura }" pattern="dd/MM/yyyy"/> </li>
 		</c:forEach>
 	</ul>
-
+	
 </body>
 </html>
